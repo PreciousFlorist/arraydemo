dc5e33c5703cdf829abd4e9643fd3a60051a5783
Shane Walders <shanewalders@gmail.com>
2023-12-02 22:35:37 -0800

    Structured and styled social media logo array

---
+++ src/lib/components/assets/SocialMediaIcon.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let image;
+	export let alt;
+	export let link;
+	export let size = 'default';
+</script>
+
+<a
+	href={link}
+	class="group cursor-pointer flex justify-center items-center w-full h-full relative overflow-hidden after:absolute after:bg-whitesmoke after:h-full after:top-full after:bottom-0 after:right-0 after:left-0 after:transition-all after:duration-300 hover:after:top-0"
+>
+	<img
+		src={image}
+		{alt}
+		class="transition-all duration-300 relative z-10 grayscale brightness-50 group-hover:brightness-100 group-hover:grayscale-0"
+		style={`width: ${size === 'large' ? '32px' : '17px'}`}
+	/>
+</a>
