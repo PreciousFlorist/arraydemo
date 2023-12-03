9c178b69a8951a54d5b525337d793c5eb2865f8c
Shane Walders <shanewalders@gmail.com>
2023-12-02 22:25:46 -0800

    Added header navigation
    - Developed seperate components for desktop and mobile
    - Added JSON data structure for content managment
    - Added GSAP animations

---
+++ src/lib/layout/MobileNavbar.svelte
@@ -0,0 +1,193 @@
+<script>
+	import gsap from 'gsap';
+	import { isValidArray } from '$lib/utils/validation';
+
+	import SocialMediaIcon from '$lib/components/assets/SocialMediaIcon.svelte';
+	import Button from '$lib/components/assets/Button.svelte';
+	import { toggleContactModal } from '$lib/utils/contactForm.js';
+
+	// Process CMS content
+	export let header;
+	let navigationLinks = header?.navigationLinks || [];
+	let socialLinks = header?.socialLinks || [];
+	let cta = header?.cta || [];
+
+	let active = false;
+	let submenuElements = [];
+
+	function openContactModal() {
+		toggleContactModal();
+		toggleMenu();
+	}
+
+	function toggleMenu() {
+		active = !active;
+
+		// Close all submenus along with nav
+		if (!active) {
+			submenuElements.forEach((element) => {
+				gsap.delayedCall(0.3, () => {
+					gsap.set(element, { x: 0 });
+				});
+			});
+		}
+	}
+
+	function toggleSubmenu(index) {
+		const element = submenuElements[index];
+		if (element) {
+			if (gsap.getProperty(element, 'x') !== 0) {
+				// Close
+				gsap.fromTo(element, { x: '-100%' }, { x: 0, duration: 0.3 });
+			} else {
+				// Open
+				gsap.fromTo(element, { x: 0 }, { x: '-100%', duration: 0.3 });
+			}
+		}
+	}
+</script>
+
+<header
+	class="950:hidden fixed w-full flex flex-row justify-between items-center top-0 z-20 pl-[24px] h-[64px] border-b border-silver backdrop-blur after:w-full after:h-full after:bg-white after:top-0 after:left-0 after:opacity-80 after:absolute"
+>
+	<!-- Logo -->
+	<div class="relative z-30">
+		<img src="/images/array-logo.svg" alt="Array Logo" class="h-43px" />
+	</div>
+	<!-- Hamburger -->
+	<button
+		class="absolute z-30 right-0 p-15px h-full w-[64px] border-l border-silver bg-whitesmoke flex justify-center items-center"
+		on:click={toggleMenu}
+	>
+		<div>
+			<span
+				class={`my-[8px] ml-auto h-0.5 w-4 block rounded-full transition-all duration-300 origin-[50%_50%] ${
+					active
+						? 'translate-y-[10px] -translate-x-[0px] w-7 rotate-[135deg] bg-egyptianblue'
+						: 'bg-jet'
+				} `}
+			></span>
+			<span
+				class={`my-[8px] ml-auto h-0.5 w-7 block rounded-full transition-all duration-300 origin-[50%_50%] ${
+					active ? 'scale-0' : 'bg-jet'
+				}`}
+			></span>
+			<span
+				class={`my-[8px] ml-auto h-0.5 w-4 block rounded-full transition-all duration-300 origin-[50%_50%] ${
+					active
+						? '-translate-y-[10px] -translate-x-[0px] w-7 -rotate-[135deg] bg-egyptianblue'
+						: 'bg-jet'
+				} `}
+			></span>
+		</div>
+	</button>
+	<!-- Navigation -->
+	<nav
+		class={`fixed z-40 top-full h-[calc(100vh-64px)] right-0 left-0 bg-white border-b border-t border-silver transition-all duration-300 overflow-x-hidden overflow-y-scroll ${
+			active ? 'visible opacity-1' : 'invisible opacity-0'
+		}`}
+	>
+		<div class="flex z-40">
+			<!-- Page Links -->
+			{#if isValidArray(navigationLinks)}
+				<ul class="pl-[24px] py-5 grow">
+					{#each navigationLinks as link, index}
+						{#if link.type === 'link'}
+							<li class="py-2.5">
+								<a class="text-jet text-[19px] font-semibold uppercase tracking-1px" href={link.url}
+									>{link.title}</a
+								>
+							</li>
+						{:else if link.type === 'dropdown'}
+							<li class=" py-2.5">
+								<button
+									class="cursor-pointer text-jet text-[19px] font-semibold uppercase tracking-1px"
+									on:click={() => toggleSubmenu(index)}>{link.title} +</button
+								>
+							</li>
+						{/if}
+					{/each}
+				</ul>
+			{/if}
+
+			<div class="w-[64px] border-l border-silver"></div>
+		</div>
+		<!-- Search -->
+		<div class="border-t border-b border-silver flex h-[64px] z-40">
+			<input
+				class="grow pl-[24px] text-[18px] tracking-1px font-light"
+				type="text"
+				placeholder="Search our site"
+			/>
+			<div class="w-[64px] flex justify-center items-center">
+				<img src="/images/search-icon.svg" alt="Search" class="cursor-pointer" />
+			</div>
+		</div>
+		<!-- CTA -->
+		{#if isValidArray(cta)}
+			{#each cta as banner}
+				<div
+					class={`py-10 z-40 px-[24px] flex flex-col justify-center items-center ${banner.background}`}
+				>
+					<p class="font-serif text-white text-4xl mb-6 text-center">{banner.title}</p>
+					<Button text={banner.buttonText} onClick={openContactModal} />
+				</div>
+			{/each}
+		{/if}
+		<!-- Social Links -->
+		<ul class="flex flex-row z-40 border-b border-silver">
+			{#each socialLinks as social}
+				<li class="w-1/4 border-l border-silver flex justify-center items-center aspect-square">
+					<SocialMediaIcon image={social.image} alt={social.alt} link={social.link} size="large" />
+				</li>
+			{/each}
+		</ul>
+		<!-- Submenu -->
+		{#if isValidArray(navigationLinks)}
+			{#each navigationLinks as link, index}
+				{#if link.type === 'dropdown'}
+					<div
+						bind:this={submenuElements[index]}
+						class="absolute z-40 w-full h-[calc(100vh-64px)] top-0 bg-white border-b border-silver left-full z-10 overflow-scroll"
+					>
+						<div class="py-4 px-[24px] flex justify-between border-b border-silver">
+							<p class="text-lg font-bold">{link.title}</p>
+							<button
+								on:click={() => toggleSubmenu(index)}
+								class="text-egyptianblue uppercase font-semibold text-sm tracking-1px flex gap-2.5 items-center"
+							>
+								<img src="/images/assets/arrow-blue.svg" alt="Left arrow" />
+
+								<span class="relative top-1px">Back</span>
+							</button>
+						</div>
+						<ul class="grid grid-cols-2">
+							{#each link.dropdownContent.items as item}
+								<li
+									class="border-b border-r border-silver px-[24px] p-5 flex flex-col gap-2.5 justify-center items-center"
+								>
+									<img src={item.image} alt={item.alt} class="w-16 relative" />
+									<p
+										class="text-center uppercase text-jet text-semibold tracking-1px text-xs"
+										href="#"
+									>
+										{item.title}
+									</p>
+								</li>
+							{/each}
+						</ul>
+
+						<div
+							class={`py-10 px-[24px] flex flex-col gap-2 ${link.dropdownContent.background} bg-cover`}
+						>
+							<p class="font-serif text-white text-3xl tracking-1px">{link.cta.title}</p>
+							<p class="font-sans text-white text-base tracking-1px font-light">
+								{link.cta.description}
+							</p>
+						</div>
+					</div>
+				{/if}
+			{/each}
+		{/if}
+	</nav>
+</header>
---
+++ src/lib/layout/Navbar.svelte
@@ -0,0 +1,168 @@
+<script>
+	import { isValidArray } from '$lib/utils/validation';
+	import Link from '$lib/components/assets/Link.svelte';
+	import SocialMediaIcon from '$lib/components/assets/SocialMediaIcon.svelte';
+
+	// Process CMS content
+	export let header;
+	let navigationLinks = header?.navigationLinks || [];
+	let socialLinks = header?.socialLinks || [];
+
+	// Toggle search bar visibility
+	let isSearchOpen = false;
+	function toggleSearch() {
+		isSearchOpen = !isSearchOpen;
+	}
+</script>
+
+<header
+	class="hidden 950:flex fixed w-full flex-row justify-between items-center pl-20 border-b border-silver backdrop-blur z-20 after:absolute after:w-full after:h-full after:bg-white after:top-0 after:left-0 after:opacity-80"
+>
+	<!-- Logo -->
+	<div class="relative z-30">
+		<img src="/images/array-logo.svg" alt="Array Logo" class="h-43px" />
+	</div>
+
+	<!-- Main Navigation -->
+	<nav class=" z-30 flex flex-row">
+		<!-- Page Links -->
+		<ul class="flex flex-row">
+			{#if isValidArray(navigationLinks)}
+				{#each navigationLinks as link}
+					<!-- Standard links -->
+					{#if link.type === 'link'}
+						<li
+							class={`p-15px xl:px-25px flex items-center uppercase text-onyx font-semibold tracking-1px cursor-pointer group/link duration-200 ${
+								isSearchOpen ? 'opacity-0' : ''
+							}`}
+						>
+							<p
+								class="text-sm 1100:text-base transition-all group-hover/link:text-coral whitespace-nowrap"
+							>
+								{link.title}
+							</p>
+						</li>
+						<!-- Links with submenus -->
+					{:else if link.type === 'dropdown'}
+						<li
+							class={`p-15px xl:px-25px flex items-center uppercase text-onyx font-semibold tracking-1px cursor-default group/dropdown duration-200
+						${isSearchOpen ? 'opacity-0' : ''}
+						`}
+						>
+							<p
+								class="text-sm 1100:text-base transition-all group-hover/dropdown:text-coral whitespace-nowrap"
+							>
+								{link.title}
+							</p>
+
+							{#if link.dropdownContent}
+								<div
+									class={`absolute top-full w-full pl-14 1100:pl-20 right-0 l-14 border-t border-silver flex transition-all duration-300 group-hover/dropdown:opacity-100 group-hover/dropdown:visible opacity-0 invisible`}
+								>
+									<!-- Submenu greeting -->
+									<div
+										class={`flex flex-col w-1/4 justify-center items-center ${
+											link.dropdownContent.items && link.dropdownContent.items.length >= 6
+												? ' min-w-[315px] 1100:min-w-[340px] col-span-4 1100:col-span-1 row-span-2'
+												: ' min-w-[265px] 1100:min-w-[350px] col-span-2'
+										} py-2.5 px-6 1100:pl-16 bg-white border-b border-silver`}
+									>
+										<div class="flex flex-col gap-15px">
+											{#if link.dropdownContent.header.title}
+												<p class="capitalize font-serif text-[22px] 1100:text-[28px] leading-10">
+													{link.dropdownContent.header.title}
+												</p>
+											{/if}
+											<div>
+												{#if link.dropdownContent.header.description}
+													<p
+														class="capitalize mb-8 text-[17px] 1100:text-[22px] font-light leading-8"
+													>
+														{link.dropdownContent.header.description}
+													</p>
+												{/if}
+
+												{#if link.dropdownContent.header.actionText && link.dropdownContent.header.actionLink}
+													<Link
+														url={link.dropdownContent.header.actionLink}
+														text={link.dropdownContent.header.actionText}
+														color="blue"
+													/>
+												{/if}
+											</div>
+										</div>
+									</div>
+
+									<!-- Submenu pages -->
+									<div class={`flex grow flex-wrap bg-cover ${link.dropdownContent.background} `}>
+										{#each link.dropdownContent.items as cell}
+											<div
+												class={`group/cell py-2.5 px-[25px] cursor-pointer flex flex-col justify-center items-center gap-y-3.5 relative overflow-hidden border-b border-l border-silver ${
+													link.dropdownContent.items && link.dropdownContent.items.length >= 6
+														? 'w-1/3 h-[200px]'
+														: 'w-1/5 h-[255px] 1100:h-[340px]'
+												}`}
+											>
+												<div
+													class="absolute bg-white h-full w-[102%] top-0 transition-all duration-300 z-0 group-hover/cell:-translate-y-full py-2.5 px-[25px]"
+												></div>
+
+												{#if cell.image && cell.alt}
+													<div class="h-[70px] flex items-center">
+														<img
+															src={cell.image}
+															alt={cell.alt}
+															class="w-16 relative z-10 transition-all duration-300 filter-none group-hover/cell:brightness-0 group-hover/cell:invert"
+														/>
+													</div>
+												{/if}
+												{#if cell.title}
+													<p
+														class={`font-sans text-center relative z-10 transition-colors duration-300 text-[14px] 1100:text-[16px] group-hover/cell:text-white max-w-180px ${
+															link.dropdownContent.items && link.dropdownContent.items.length >= 6
+																? ''
+																: 'whitespace-nowrap'
+														}`}
+													>
+														{cell.title}
+													</p>
+												{/if}
+											</div>
+										{/each}
+									</div>
+								</div>
+							{/if}
+						</li>
+					{/if}
+				{/each}
+			{/if}
+			<!-- Search Bar -->
+			<li class="px-3.5 1100:py-6 1100:pr-10 relative flex items-center">
+				<button class="relative z-30 flex items-center py-3px" on:click={toggleSearch}>
+					<img src="/images/search-icon.svg" alt="Search" class="cursor-pointer" /></button
+				>
+				<input
+					type="text"
+					placeholder="What are you looking for?"
+					class={`py-1 pr-2.5 1100:pl-12 font-sans font-extralight absolute h-full top-0 bottom-0 transition-all duration-300 z-20 bg-transparent after:duration-200 ${
+						isSearchOpen
+							? 'border-l border-silver right-12 w-500px opacity-100'
+							: 'right-0 opacity-0 w-0'
+					}`}
+				/>
+			</li>
+		</ul>
+		<!-- Socials -->
+		{#if isValidArray(socialLinks)}
+			<ul class="flex flex-row relative z-30 min-h-[77px]">
+				{#each socialLinks as social}
+					{#if social.image && social.alt && social.link}
+						<li class="w-[54px] flex justify-center items-center border-l border-silver">
+							<SocialMediaIcon image={social.image} alt={social.alt} link={social.link} />
+						</li>
+					{/if}
+				{/each}
+			</ul>
+		{/if}
+	</nav>
+</header>
